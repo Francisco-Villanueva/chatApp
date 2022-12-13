2193b7bee3e97925e6c5718a4a48f39ccbfd1103
unknown <franciscovillanuevaj99@gmail.com>
2022-12-13 13:39:41 -0300

    second commit

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -7,11 +7,15 @@ import {useAuthState} from 'react-firebase-hooks/auth'
 export default function User() {
     const [user] = useAuthState(auth)
     console.log(user)
+
+    const userImg = user? user.photoURL : 'https://bysperfeccionoral.com/wp-content/uploads/2020/01/136-1366211_group-of-10-guys-login-user-icon-png.jpg'
+    const userName = user? user.displayName : 'User Namer'
   return (
     <div className='right-side'>
         <h1>Chat app</h1>
         <article className='card-user'>
-            <p>UserName</p>
+            <img src={userImg} alt='user photo'/>
+            <p>{userName}</p>
             {user? <LogOut/> : <LogIn/>}
         </article>
         
