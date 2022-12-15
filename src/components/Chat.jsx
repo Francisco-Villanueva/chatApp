3636dc2e580bbf97d9c28cdba1cbb374d6468557
Francisco Villanueva <franciscovillanuevaj99@gmail.com>
2022-12-15 02:18:19 -0300

    styling 2

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,42 +1,36 @@
-    import React from 'react'
-    import {auth, db} from '../firebase'
-    import {query, collection, orderBy, onSnapshot} from 'firebase/firestore'
-    import { useState } from 'react'
-import { useEffect } from 'react'
-import Message from './Message'
-    
+import React from "react";
+import { auth, db } from "../firebase";
+import { query, collection, orderBy, onSnapshot } from "firebase/firestore";
+import { useState } from "react";
+import { useEffect } from "react";
+import Message from "./Message";
+import "./styles/Chat.css";
 
-    export default function Chat() {
+export default function Chat() {
+  const [message, setMessage] = useState([]);
 
-        const[message, setMessage] = useState([])
+  useEffect(() => {
+    const newQuery = query(collection(db, "messages"), orderBy("timestamp"));
 
-        useEffect(()=>{
-            const newQuery = query(collection(db, "messages" ), orderBy('timestamp'))
+    const unsubscribe = onSnapshot(newQuery, (querySnapshot) => {
+      let currentMsg = [];
 
-            const unsubscribe = onSnapshot(newQuery, (querySnapshot)=>{
-                let currentMsg = []
-                
-                querySnapshot.forEach(e=>{
-                    currentMsg.push({content: e.data(), id: e.id})
-                    // console.log(e.data())
-                })
+      querySnapshot.forEach((e) => {
+        currentMsg.push({ content: e.data(), id: e.id });
+        // console.log(e.data())
+      });
 
-                setMessage(currentMsg)
-            })
+      setMessage(currentMsg);
+    });
 
-            return unsubscribe
-        },[])
-    return (
-        <div>
-            <section className='chat-container'>
-                {message && message.map(m=>(
-                    <Message 
-                    key={m.id}
-                    msg={m.content}
-                        />
-                ))}
-                <h4>This is a message</h4>
-            </section>
-        </div>
-    )
-    }
+    return unsubscribe;
+  }, []);
+  return (
+    <div className="chat-main">
+      <section className="chat-container">
+        {message && message.map((m) => <Message key={m.id} msg={m.content} />)}
+        <h4>This is a message</h4>
+      </section>
+    </div>
+  );
+}
--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,27 +1,30 @@
-import React from 'react'
-import LogIn from './LogIn'
-import LogOut from './LogOut'
-import {auth} from '../firebase'
-import {useAuthState} from 'react-firebase-hooks/auth'
-import './styles/User.css'
+import React from "react";
+import LogIn from "./LogIn";
+import LogOut from "./LogOut";
+import { auth } from "../firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import "./styles/User.css";
 
 export default function User() {
-    const [user] = useAuthState(auth)
-    console.log(user)
+  const [user] = useAuthState(auth);
+  console.log(user);
 
-    const userImg = user? user.photoURL : 'https://bysperfeccionoral.com/wp-content/uploads/2020/01/136-1366211_group-of-10-guys-login-user-icon-png.jpg'
-    const userName = user? user.displayName : 'User Namer'
+  const userImg = user
+    ? user.photoURL
+    : "https://cdn-icons-png.flaticon.com/512/149/149071.png";
+  const userName = user ? user.displayName : "User Namer";
   return (
-    <div className='user-main'>
-        <h1>Chat app</h1>
-        <article className='card-user'>
-            <div className='user-data'>
-              <img src={userImg} alt='user photo'/>
-              <p>{userName}</p>
-            </div>
-            {user? <LogOut/> : <LogIn/>}
-        </article>
-        
+    <div className="user-main">
+      <div className="app-title">
+        <h3>Rapi-chat</h3>
+      </div>
+      <article className="card-user">
+        <div className="user-data">
+          <img src={userImg} alt="user photo" />
+          <p>{userName}</p>
+        </div>
+      </article>
+      {user ? <LogOut /> : <LogIn />}
     </div>
-  )
+  );
 }
