a8c4047c24513da39498010a88e2404fbfef8c93
Francisco Villanueva <franciscovillanuevaj99@gmail.com>
2022-12-16 10:42:34 -0300

    agregue clases mateo

---
+++ src/clases/test.js
@@ -0,0 +1,25 @@
+//FUNCIONES
+
+function sumarDosNum(params1, params2) {
+  let suma = params1 + params2;
+
+  return suma;
+}
+
+const suma = (a, b) => {
+  let suma = a + b;
+  return suma;
+};
+
+//CICLOS for
+
+for (let i = 0; i < 6; i++) {
+  return i;
+}
+
+let aux = [];
+const bucle = () => {
+  for (let i = 0; i < 6; i++) {
+    console.log(i);
+  }
+};
